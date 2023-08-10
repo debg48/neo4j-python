ba346b2f247cdc3f667a11919d3905ffc5031902
debg48 <debgandhar4000@gmail.com>
2023-08-10 11:34:16 +0530

    db populated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from tqdm import tqdm
 import json
 from neo4j import GraphDatabase
+import time
 
 file = "./arxiv-metadata-oai-snapshot.json"
 
@@ -84,3 +85,94 @@ class Neo4jConnection:
 conn = Neo4jConnection(uri="bolt://54.165.178.82:7687", 
                        user="neo4j",              
                        pwd="bails-string-transfers")
+
+# query neo4j using python and populate the database
+
+conn.query('CREATE CONSTRAINT papers IF NOT EXISTS FOR (p:Paper)  REQUIRE p.id IS UNIQUE')
+conn.query('CREATE CONSTRAINT authors IF NOT EXISTS FOR (a:Author) REQUIRE a.name IS UNIQUE')
+conn.query('CREATE CONSTRAINT categories IF NOT EXISTS FOR (c:Category) REQUIRE c.category IS UNIQUE')
+
+
+def add_categories(categories):
+    # Adds category nodes to the Neo4j graph.
+    query = '''
+            UNWIND $rows AS row
+            MERGE (c:Category {category: row.category})
+            RETURN count(*) as total
+            '''
+    return conn.query(query, parameters = {'rows':categories.to_dict('records')})
+
+
+def add_authors(rows, batch_size=100):
+    # Adds author nodes to the Neo4j graph as a batch job.
+    query = '''
+            UNWIND $rows AS row
+            MERGE (:Author {name: row.author})
+            RETURN count(*) as total
+            '''
+    return insert_data(query, rows, batch_size)
+
+
+def insert_data(query, rows, batch_size = 100):
+    # Function to handle the updating the Neo4j database in batch mode.
+    
+    total = 0
+    batch = 0
+    start = time.time()
+    result = None
+    
+    while batch * batch_size < len(rows):
+
+        res = conn.query(query, 
+                         parameters = {'rows': rows[batch*batch_size:(batch+1)*batch_size].to_dict('records')})
+        total += res[0]['total']
+        batch += 1
+        result = {"total":total, 
+                  "batches":batch, 
+                  "time":time.time()-start}
+        print(result)
+        
+    return result
+
+# paper node 
+
+def add_papers(rows, batch_size=5000):
+   # Adds paper nodes and (:Author)--(:Paper) and 
+   # (:Paper)--(:Category) relationships to the Neo4j graph as a 
+   # batch job.
+ 
+   query = '''
+   UNWIND $rows as row
+   MERGE (p:Paper {id:row.id}) ON CREATE SET p.title = row.title
+ 
+   // connect categories
+   WITH row, p
+   UNWIND row.category_list AS category_name
+   MATCH (c:Category {category: category_name})
+   MERGE (p)-[:IN_CATEGORY]->(c)
+ 
+   // connect authors
+   WITH distinct row, p // reduce cardinality
+   UNWIND row.cleaned_authors_list AS author
+   MATCH (a:Author {name: author})
+   MERGE (a)-[:AUTHORED]->(p)
+   RETURN count(distinct p) as total
+   '''
+ 
+   return insert_data(query, rows, batch_size)
+
+
+categories = pd.DataFrame(df[['category_list']])
+categories.rename(columns={'category_list':'category'},
+                  inplace=True)
+categories = categories.explode('category') \
+                       .drop_duplicates(subset=['category'])
+
+authors = pd.DataFrame(df[['cleaned_authors_list']])
+authors.rename(columns={'cleaned_authors_list':'author'},
+               inplace=True)
+authors=authors.explode('author').drop_duplicates(subset=['author'])
+
+add_categories(categories)
+add_authors(authors)
+add_papers(df)
